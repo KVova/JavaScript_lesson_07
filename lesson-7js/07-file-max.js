4e094e3aca77fbb39e3c3d2b4d830c10fae42c82
K_Vova <karmazynvolodymyr>
2020-04-06 19:58:04 +0300

    est

--- lesson-7js/07-file-max.js
+++ lesson-7js/07-file-max.js
@@ -4,7 +4,8 @@ function SelectLink() {
 	var elements = document.querySelectorAll('ul>li>a');
 	for (var i = 0; i < elements.length; i++) {
 		var exElements = elements[i].innerHTML.split('//');
-		if(exElements[0] == 'http:' || exElements[0] == 'ftp:' || exElements[0] == 'http://internal.com'){
+		if (exElements[0] == 'http:' || exElements[0] == 'ftp:'
+				|| exElements[0] == 'http://internal.com') {
 			elements[i].className = 'external-red';
 			console.log(exElements);
 		}
